d65c805214efdb771eb96c27432dcb09c7812867
Suhailie <13517045@std.stei.itb.ac.id>
2023-08-12 18:54:04 +0700

    first commit

---
+++ demo/src/main/java/net/login/springboot/SpringbootLoginApiApplication.java
@@ -0,0 +1,13 @@
+package net.login.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootLoginApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootLoginApiApplication.class, args);
+	}
+
+}
---
+++ demo/src/test/java/net/login/springboot/SpringbootLoginApiApplicationTests.java
@@ -0,0 +1,13 @@
+package net.login.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootLoginApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
